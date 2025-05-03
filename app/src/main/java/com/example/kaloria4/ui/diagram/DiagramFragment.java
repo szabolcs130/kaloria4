2240599e3e8f71368c01648ae00625db7b6f9640
alma <a@gamil.com>
2025-05-03 16:52:31 +0200

    diagramNapiKalorianal datum jelenik meg, dao maxkaloria javit

--- app/src/main/java/com/example/kaloria4/ui/diagram/DiagramFragment.java
+++ app/src/main/java/com/example/kaloria4/ui/diagram/DiagramFragment.java
@@ -23,6 +23,7 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,56 +53,58 @@ public class DiagramFragment extends Fragment {
 
         return root;
     }
-public void NapiKaloriaDiagram(){
-    if (barChart!=null) {
-        barChart.clear();
-    }
-    if (entries!=null) {
-        entries.clear();
-    }
+    public void NapiKaloriaDiagram(){
+        if (barChart!=null) {
+            barChart.clear();
+        }
+        if (entries!=null) {
+            entries.clear();
+        }
    /* etkezesViewModel.getAllEtkezesMaxKaloria().observe(getViewLifecycleOwner(), etkezesek -> {
         for (int i=0;i<etkezesek.size();i++){
             System.out.println("max");
             System.out.println(etkezesek.get(i));
         }
     });*/
-     etkezesViewModel.getAllEtkezesMaxKaloria().observe(getViewLifecycleOwner(), etkezesek -> {
-        maxKaloria=etkezesek.get(0).getKaloria();
-        // System.out.println("Max kaloria "+maxKaloria);
-     });
-    etkezesViewModel.getAllEtkezesMaxKaloria().observe(getViewLifecycleOwner(), etkezesek -> {
+        etkezesViewModel.getAllEtkezesMaxKaloriaMax().observe(getViewLifecycleOwner(), etkezesek -> {
+            maxKaloria=etkezesek.get(0).getKaloria();
+            // System.out.println("Max kaloria "+maxKaloria);
+        });
+        etkezesViewModel.getAllEtkezesMaxKaloria().observe(getViewLifecycleOwner(), etkezesek -> {
 
-        for (int i = 0; i < etkezesek.size(); i++) {
-            EtkezesOsszevont fogas = etkezesek.get(i);
+            for (int i = 0; i < etkezesek.size(); i++) {
+                EtkezesOsszevont fogas = etkezesek.get(i);
            /* if (maxKaloria<fogas.getKaloria()){
                 maxKaloria=fogas.getKaloria();
             }*/
-            entries.add(new BarEntry(i, (float) fogas.getKaloria()));
-           // System.out.println("Kaloriak: "+fogas.getKaloria());
-        }
-        YAxis yAxis = barChart.getAxisLeft();
-        yAxis.setAxisMaximum((float)maxKaloria);
-        yAxis.setAxisMinimum(0f);
-        yAxis.setAxisLineWidth(2f);
-        yAxis.setAxisLineColor(Color.BLACK);
-        yAxis.setLabelCount(10);
-        BarDataSet dataSet = new BarDataSet(entries, "Nap Kaloria Adatok");
-        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
-        barData = new BarData(dataSet);
-        barChart.setData(barData);
-        List<String> xValues = new ArrayList<>();
-        for (EtkezesOsszevont etkezes : etkezesek) {
-            xValues.add(String.valueOf(etkezes.getKaloria()));
-        }
-        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(xValues));
-        barChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
-        barChart.getXAxis().setGranularity(1f);
-        barChart.getXAxis().setGranularityEnabled(true);
+                entries.add(new BarEntry(i, (float) fogas.getKaloria()));
+                // System.out.println("Kaloriak: "+fogas.getKaloria());
+            }
+            YAxis yAxis = barChart.getAxisLeft();
+            yAxis.setAxisMaximum((float)maxKaloria);
+            yAxis.setAxisMinimum(0f);
+            yAxis.setAxisLineWidth(2f);
+            yAxis.setAxisLineColor(Color.BLACK);
+            yAxis.setLabelCount(10);
+            BarDataSet dataSet = new BarDataSet(entries, "Nap Kaloria Adatok");
+            dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+            barData = new BarData(dataSet);
+            barChart.setData(barData);
+            List<String> xValues = new ArrayList<>();
+            for (EtkezesOsszevont etkezes : etkezesek) {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                String formattedDate = sdf.format(etkezes.getEtkezesIdopontIdo());
+                xValues.add(String.valueOf(formattedDate));
+            }
+            barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(xValues));
+            barChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+            barChart.getXAxis().setGranularity(1f);
+            barChart.getXAxis().setGranularityEnabled(true);
 
-        barChart.getDescription().setEnabled(false);
-        barChart.invalidate();
-    });
-}
+            barChart.getDescription().setEnabled(false);
+            barChart.invalidate();
+        });
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
